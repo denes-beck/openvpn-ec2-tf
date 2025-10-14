51624648d4bf59ffc1439d1b70b33a7f28bb0876
denesbeck <denes.beck1@outlook.com>
2025-10-14 20:05:15 +0200

    refactor: use spot instances

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,12 @@ resource "aws_instance" "openvpn_server" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.openvpn_sg.id]
   key_name               = aws_key_pair.openvpn_kp.key_name
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      max_price = 0.015
+    }
+  }
   tags = {
     Name = "my-vpn"
   }
