57401c76d5bd91cefc33958bb075b3775f9fad00
denesbeck <denes.beck1@outlook.com>
2025-10-14 20:05:15 +0200

    refactor: rm deprecated `inline_policy` from `aws_iam_role`

--- iam-role.tf
+++ iam-role.tf
@@ -2,7 +2,6 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "openvpn_role" {
   name = "openvpn-ec2-switch-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -15,22 +14,23 @@ resource "aws_iam_role" "openvpn_role" {
       }
     ]
   })
+}
 
-  inline_policy {
-    name = "openvpn-ec2-policy"
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Effect = "Allow"
-          Action = [
-            "ec2:StartInstances",
-            "ec2:StopInstances"
-          ]
-          Resource = "arn:aws:ec2:${var.region}:${data.aws_caller_identity.current.account_id
-          }:instance/${aws_instance.openvpn_server.id}"
-        }
-      ]
-    })
-  }
+resource "aws_iam_role_policy" "openvpn_ec2_policy" {
+  name = "openvpn-ec2-policy"
+  role = aws_iam_role.openvpn_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:StartInstances",
+          "ec2:StopInstances"
+        ]
+        Resource = "arn:aws:ec2:${var.region}:${data.aws_caller_identity.current.account_id
+        }:instance/${aws_instance.openvpn_server.id}"
+      }
+    ]
+  })
 }
